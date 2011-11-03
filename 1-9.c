02323d0ea3430d9187c14030a3df8f1320ac8eb6
Matthew McPherrin <matthew@mcpherrin.ca>
2011-11-02 21:53:52 -0400

    Exercise 109

---
+++ 1-9.c
@@ -0,0 +1,18 @@
+/* K&R Exercise 1-9: Write a program to copy its input to its output, replacing
+ * each string of one or more blanks by a single blank */
+
+ #include <stdio.h>
+
+ main
+ {
+	int c, inblank;
+	inblank = 0;
+	while(( c=getchar() ) != EOF ) {
+		if(c != ' ') {
+			putchar(c);
+		} else if(!inblank) {
+			inblank = 1;
+			putchar(' ');
+		}
+	}
+}
